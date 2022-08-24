95d6cc19945dd3bfa4c2b13c73aa47fc7322eeac
LoLeo <101693004+loraleonida@users.noreply.github.com>
2022-08-23 18:53:20 -0700

    Indv and Pac Heatmaps

--- web app/static/js/past_data_app.js
+++ web app/static/js/past_data_app.js
@@ -1,4 +1,4 @@
-/////////////// PAST DATA HEATMAP ///////////////
+/////////////// PAST DATA TOTAL HEATMAP ///////////////
 d3.csv("https://raw.githubusercontent.com/armyofkittens/Analyze-This-Campaign-Finance-Analysis/Lora/Past%20Data/cycle_year.csv", function(err, rows){
 
   function filter_and_unpack(rows, key, year) {
@@ -115,11 +115,254 @@ var heatLayout = {
     }]
 };
 
-Plotly.newPlot('contribution_heatmap', heatData, heatLayout).then(function() {
-    Plotly.addFrames('contribution_heatmap', frames);
+Plotly.newPlot('contribution_total_heatmap', heatData, heatLayout).then(function() {
+    Plotly.addFrames('contribution_total_heatmap', frames);
   });
 })
-  
+
+/////////////// PAST DATA INDIVIDUALS HEATMAP ///////////////
+d3.csv("https://raw.githubusercontent.com/armyofkittens/Analyze-This-Campaign-Finance-Analysis/Lora/Past%20Data/cycle_year.csv", function(err, rows){
+
+  function filter_and_unpack(rows, key, year) {
+  return rows.filter(row => row['cycle'] == year).map(row => row[key])
+  }
+
+  var frames = []
+  var slider_steps = []
+
+  var n = 13;
+  var num = 1990;
+  for (var i = 0; i <= n; i++) {
+    var z = filter_and_unpack(rows, 'raised_from_individuals', num)
+    var locations = filter_and_unpack(rows, 'state', num)
+    frames[i] = {data: [{z: z, locations: locations, text: locations}], name: num}
+    slider_steps.push ({
+        label: num.toString(),
+        method: "animate",
+        args: [[num], {
+            mode: "immediate",
+            transition: {duration: 300},
+            frame: {duration: 300}
+          }
+        ]
+      })
+    num = num + 2
+  }
+
+var heatData = [{
+      type: 'choropleth',
+      locationmode: 'USA-states',
+      locations: frames[0].data[0].locations,
+      z: frames[0].data[0].z,
+      text: frames[0].data[0].locations,
+      zauto: false,
+      zmin: 30,
+      zmax: 10000000,
+      colorscale: 'RdBu'
+
+}];
+var heatLayout = {
+    title: 'Individual Contributions By State<br>1990 - 2016',
+    geo:{
+       scope: 'usa',
+       countrycolor: 'rgb(255, 255, 255)',
+       showland: true,
+       landcolor: 'rgb(217, 217, 217)',
+       showlakes: true,
+       lakecolor: 'rgb(255, 255, 255)',
+       subunitcolor: 'rgb(255, 255, 255)',
+       lonaxis: {},
+       lataxis: {}
+    },
+    updatemenus: [{
+      x: 0.1,
+      y: 0,
+      yanchor: "top",
+      xanchor: "right",
+      showactive: false,
+      direction: "left",
+      type: "buttons",
+      pad: {"t": 87, "r": 10},
+      buttons: [{
+        method: "animate",
+        args: [null, {
+          fromcurrent: true,
+          transition: {
+            duration: 200,
+          },
+          frame: {
+            duration: 500
+          }
+        }],
+        label: "Play"
+      }, {
+        method: "animate",
+        args: [
+          [null],
+          {
+            mode: "immediate",
+            transition: {
+              duration: 0
+            },
+            frame: {
+              duration: 0
+            }
+          }
+        ],
+        label: "Pause"
+      }]
+    }],
+    sliders: [{
+      active: 0,
+      steps: slider_steps,
+      x: 0.1,
+      len: 0.9,
+      xanchor: "left",
+      y: 0,
+      yanchor: "top",
+      pad: {t: 50, b: 10},
+      currentvalue: {
+        visible: true,
+        prefix: "Year:",
+        xanchor: "right",
+        font: {
+          size: 20,
+          color: "#666"
+        }
+      },
+      transition: {
+        duration: 300,
+        easing: "cubic-in-out"
+      }
+    }]
+};
+
+Plotly.newPlot('contribution_ind_heatmap', heatData, heatLayout).then(function() {
+    Plotly.addFrames('contribution_ind_heatmap', frames);
+  });
+})
+
+/////////////// PAST DATA PACS HEATMAP ///////////////
+d3.csv("https://raw.githubusercontent.com/armyofkittens/Analyze-This-Campaign-Finance-Analysis/Lora/Past%20Data/cycle_year.csv", function(err, rows){
+
+  function filter_and_unpack(rows, key, year) {
+  return rows.filter(row => row['cycle'] == year).map(row => row[key])
+  }
+
+  var frames = []
+  var slider_steps = []
+
+  var n = 13;
+  var num = 1990;
+  for (var i = 0; i <= n; i++) {
+    var z = filter_and_unpack(rows, 'raised_from_pacs', num)
+    var locations = filter_and_unpack(rows, 'state', num)
+    frames[i] = {data: [{z: z, locations: locations, text: locations}], name: num}
+    slider_steps.push ({
+        label: num.toString(),
+        method: "animate",
+        args: [[num], {
+            mode: "immediate",
+            transition: {duration: 300},
+            frame: {duration: 300}
+          }
+        ]
+      })
+    num = num + 2
+  }
+
+var heatData = [{
+      type: 'choropleth',
+      locationmode: 'USA-states',
+      locations: frames[0].data[0].locations,
+      z: frames[0].data[0].z,
+      text: frames[0].data[0].locations,
+      zauto: false,
+      zmin: 30,
+      zmax: 10000000,
+      colorscale: 'RdBu'
+
+}];
+var heatLayout = {
+    title: 'PAC Contributions By State<br>1990 - 2016',
+    geo:{
+       scope: 'usa',
+       countrycolor: 'rgb(255, 255, 255)',
+       showland: true,
+       landcolor: 'rgb(217, 217, 217)',
+       showlakes: true,
+       lakecolor: 'rgb(255, 255, 255)',
+       subunitcolor: 'rgb(255, 255, 255)',
+       lonaxis: {},
+       lataxis: {}
+    },
+    updatemenus: [{
+      x: 0.1,
+      y: 0,
+      yanchor: "top",
+      xanchor: "right",
+      showactive: false,
+      direction: "left",
+      type: "buttons",
+      pad: {"t": 87, "r": 10},
+      buttons: [{
+        method: "animate",
+        args: [null, {
+          fromcurrent: true,
+          transition: {
+            duration: 200,
+          },
+          frame: {
+            duration: 500
+          }
+        }],
+        label: "Play"
+      }, {
+        method: "animate",
+        args: [
+          [null],
+          {
+            mode: "immediate",
+            transition: {
+              duration: 0
+            },
+            frame: {
+              duration: 0
+            }
+          }
+        ],
+        label: "Pause"
+      }]
+    }],
+    sliders: [{
+      active: 0,
+      steps: slider_steps,
+      x: 0.1,
+      len: 0.9,
+      xanchor: "left",
+      y: 0,
+      yanchor: "top",
+      pad: {t: 50, b: 10},
+      currentvalue: {
+        visible: true,
+        prefix: "Year:",
+        xanchor: "right",
+        font: {
+          size: 20,
+          color: "#666"
+        }
+      },
+      transition: {
+        duration: 300,
+        easing: "cubic-in-out"
+      }
+    }]
+};
+
+Plotly.newPlot('contribution_pacs_heatmap', heatData, heatLayout).then(function() {
+    Plotly.addFrames('contribution_pacs_heatmap', frames);
+  });
+})
 
   ////////////////////// LINE CHART TOTAL CONTRIBUTIONS/////////////////////////////
   var trace1 = {
